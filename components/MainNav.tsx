892b58cfdc33d596cc48c47b787029e317e795e7
Jonathan PD - Jonathan Santos <34203527+Jonathanpd@users.noreply.github.com>
2024-04-07 16:12:47 -0300

    feat: Navigation Login || openssl rand -base64 32

--- components/MainNav.tsx
+++ components/MainNav.tsx
@@ -1,18 +1,27 @@
 import Link from 'next/link'
 import React from 'react'
 import ToggleMode from './ToggleMode'
+import { getServerSession } from 'next-auth'
+import options from '@/app/api/auth/[...nextauth]/options'
 
-const MainNav = () => {
+const MainNav = async () => {
+
+  const session = await getServerSession(options)
+  //console.log(session)
+
   return (
     <div className="flex justify-between">
         <div className="flex items-center gap-2">
             <Link href="/">Dashboard</Link>
-            <Link href="/topics">Topics</Link>
+            <Link href="/topics">Topic</Link>
             <Link href="/users">Users</Link>
         </div>
         <div className="flex items-center gap-2">
-            <Link href="/">Logout</Link>
-            <ToggleMode />
+          {session 
+            ? <Link href="/api/auth/signout?callbackUrl=/">Logout</Link>
+            : <Link href="/api/auth/signin">Login</Link>
+          }
+          <ToggleMode />
         </div>
     </div>
   )
