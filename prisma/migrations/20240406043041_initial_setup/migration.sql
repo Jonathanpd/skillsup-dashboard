ec53ac21bc72d44881270065bf67e11ee50e1112
Jonathan PD - Jonathan Santos <34203527+Jonathanpd@users.noreply.github.com>
2024-04-06 22:43:54 -0300

    feat: User Model | Schema Prisma || migration Create User Table || npx prisma migrate dev

--- prisma/migrations/20240406043041_initial_setup/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to alter the column `title` on the `topic` table. The data in that column could be lost. The data in that column will be cast from `VarChar(512)` to `VarChar(255)`.
-
-*/
--- AlterTable
-ALTER TABLE `topic` MODIFY `title` VARCHAR(255) NOT NULL;
